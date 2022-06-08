7ade2b3b6285b90688804cf3f79979fe854da6b5
mahmad <ahmadqwerty123@gmail.com>
2022-06-09 01:55:59 +0500

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,numRepetitions) {
+  let stringSeries = "";
+  for (let i=1; i<=numRepetitions; i++) {
+    stringSeries += string;
+  }
+  if (numRepetitions<0) return `ERROR`;
+  else return stringSeries;
 };
 
 // Do not edit below this line
