69a2c96dfd0d1aa2a084dd93e4edb4a333040698
mahmad <ahmadqwerty123@gmail.com>
2022-06-09 04:16:32 +0500

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  for (let i=0; i<args.length; i++) {
+    for (let j=0; j<array.length; j++) {
+      //arrayString = array[j].toString(); ====> I was going for a method where if an argument constitutes a part of array element, that array element would still be removed
+      //argString = args[i].toString();
+      if (array[j]===args[i]) {
+        array.splice(j,1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
