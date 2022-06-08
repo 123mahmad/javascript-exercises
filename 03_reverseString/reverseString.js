3c9f1aaaf58260a553ff83e0a6cda28f8cb87e21
mahmad <ahmadqwerty123@gmail.com>
2022-06-09 02:38:37 +0500

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reversedString = "";
+  let reversedCharacters = [];
+  let characters = string.split("");
+  let numCharacters = characters.length;
+  for (let i=0; i<numCharacters; i++) {
+    reversedCharacters[i] = characters[numCharacters-1-i];
+  }
+  reversedString = reversedCharacters.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
