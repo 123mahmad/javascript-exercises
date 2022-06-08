3552a643fe9a2e5476078fd478f9541fe49d969b
mahmad <ahmadqwerty123@gmail.com>
2022-06-09 04:41:26 +0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstInt,secondInt) {
+  if (typeof firstInt != `number`  ||
+      typeof secondInt != `number` ||
+      firstInt < 0 ||
+      secondInt < 0 ) {
+    return `ERROR`;
+    }
 
+  if (firstInt<=secondInt) {
+    lowerInt = firstInt;
+    upperInt = secondInt;
+  } else {
+    lowerInt = secondInt;
+    upperInt = firstInt;
+  }
+
+  let sum = 0;
+  for (let i=lowerInt; i<=upperInt; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
