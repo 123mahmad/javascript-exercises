49504fec26b976c0e4953c5516efd7e3a0689006
mahmad <ahmadqwerty123@gmail.com>
2022-06-09 05:55:12 +0500

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(degreeF) {
+  let degreeC = (degreeF-32)*(5/9);
+  return Math.round(degreeC*10)/10;
 };
 
-const ctof = function() {
-
-};
+const ctof = function(degreeC) {
+  let degreeF = (9/5)*degreeC + 32;
+  return Math.round(degreeF*10)/10;
+  };
 
 // Do not edit below this line
 module.exports = {
