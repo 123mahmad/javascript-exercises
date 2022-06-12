85346758bbe9546aed0455e3b8dffcac115f1b51
mahmad <ahmadqwerty123@gmail.com>
2022-06-13 01:05:33 +0500

    Solve Exercise 09_palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let lowerStr = str.toLowerCase();
+  let alphaStrArray = lowerStr.match(/[a-z]/gi);
+  let alphaStr = alphaStrArray.join(``);
+  let alphaStrLength = alphaStr.length;
+  let reversedStr = ``;
+  for (let i = alphaStrLength; i > 0; i--) {
+    reversedStr += alphaStr.charAt(i-1);
+  }
+  return reversedStr == alphaStr;
 };
 
 // Do not edit below this line
