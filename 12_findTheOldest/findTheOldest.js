992564d718997c30945ebab070782ca8732c9f0a
mahmad <ahmadqwerty123@gmail.com>
2022-06-13 02:38:09 +0500

    Solve Exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
+  return oldest = people.reduce((oldestYet,next) => {
+    if (oldestYet.yearOfDeath == undefined) {oldestYet.yearOfDeath = currentYear};
+    if (next.yearOfDeath == undefined) {next.yearOfDeath = currentYear};
+    if ((oldestYet.yearOfDeath - oldestYet.yearOfBirth) >=
+        (next.yearOfDeath - next.yearOfBirth)) {return oldestYet};
+    return next;
+  }, people[0])
 };
 
 // Do not edit below this line
