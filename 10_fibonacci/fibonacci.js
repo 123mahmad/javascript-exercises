ca421d5dab00af6d6fdf21a15b641bbf50ca875d
mahmad <ahmadqwerty123@gmail.com>
2022-06-13 01:27:10 +0500

    Solve Exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+  if (nth<0) {return `OOPS`};
+  let sequence = [1, 1];
+  for (let i = 2; i < nth; i++) {
+    sequence[i] = sequence[i-1] + sequence[i-2];
+  }
+  return sequence[sequence.length-1];
 };
 
 // Do not edit below this line
